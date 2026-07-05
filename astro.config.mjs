757c8775ad4b267801f22eab980334cbcde02d87
Eduardo Ahumada <eduardoahumada.cactus@gmail.com>
2026-07-04 21:24:07 -0300

    Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Reemplaza con tu dominio de GitHub Pages
+    site: 'https://jetspydragon.github.io',
+    // Reemplaza con el nombre de tu repositorio si no es el principal (.github.io)
+    base: '/landingpage2',
+});
