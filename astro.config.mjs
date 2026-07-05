e057e3a4c0df55e2071d1e5417d12c63b1f0ffa2
Eduardo Ahumada <eduardoahumada.cactus@gmail.com>
2026-07-04 21:32:25 -0300

    Correct astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     // Reemplaza con tu dominio de GitHub Pages
-    site: 'https://jetspydragon.github.io',
+    site: 'https://cactusbytestudio.com',
     // Reemplaza con el nombre de tu repositorio si no es el principal (.github.io)
-    base: '/landingpage2',
+    base: '/',
 });
